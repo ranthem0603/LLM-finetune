9d4b24d63ae6aa7a486f8c9009f38a1f2f00de80
Leon1999 <rm880603@gmail.com>
2024-11-17 12:43:17 +0000

    prac2.py

--- prac2.py
+++ prac2.py
@@ -13,5 +13,11 @@ tokenized_dataset = dataset.map(lambda x: tokenizer(x['text'], truncation=True, 
 
 # Set the training param
 training_args = TrainingArguments(
+    output_dir = ".results", # Unlike push to hub, it will only save in local device
+    evaluation_strategy = "epoch", # Evaluate model performance per epoch
+    save_strategy = "epoch", # Save the model per epoch
+    learning_rate = 2e-5, 
+    per_device_train_batch_size = 16,  # Batch size
+    num_train_epochs = 3, 
   
 )
